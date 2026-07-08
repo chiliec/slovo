b876761d65e904f4c6e090100d046948e7f8a9d2
Vladimir Babin <vovababin@gmail.com>
2026-07-08 15:47:17 +0800

    feat: xp calculator

---
+++ composeApp/src/commonTest/kotlin/com/axveer/slovo/domain/XpCalculatorTest.kt
@@ -0,0 +1,11 @@
+package com.axveer.slovo.domain
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class XpCalculatorTest {
+    @Test fun session_xp_is_per_correct_plus_bonus() {
+        assertEquals(20, XpCalculator.sessionXp(0))
+        assertEquals(50, XpCalculator.sessionXp(3))
+    }
+}
