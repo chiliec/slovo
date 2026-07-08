97d3eb5d69019cbbb75d545a13f3ae0b3049116b
Vladimir Babin <vovababin@gmail.com>
2026-07-08 15:29:03 +0800

    feat: mastery calculator

---
+++ composeApp/src/commonMain/kotlin/com/axveer/slovo/domain/MasteryCalculator.kt
@@ -0,0 +1,13 @@
+package com.axveer.slovo.domain
+
+import kotlin.math.roundToInt
+
+object MasteryCalculator {
+    fun isMastered(p: CardProgress?): Boolean = (p?.correct ?: 0) > 0
+
+    fun percent(cardIds: List<String>, progress: Map<String, CardProgress>): Int {
+        if (cardIds.isEmpty()) return 0
+        val mastered = cardIds.count { isMastered(progress[it]) }
+        return (mastered.toDouble() / cardIds.size * 100).roundToInt()
+    }
+}
