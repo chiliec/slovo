0044d546b55ac516182355176a6c853627c8886c
Vladimir Babin <vovababin@gmail.com>
2026-07-08 15:21:50 +0800

    chore: gradle scaffold for Slovo KMP module

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "Slovo"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
